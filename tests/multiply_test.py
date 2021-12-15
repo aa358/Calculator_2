9fb9f0917f3136161cd9d4bffee68e05b0b2f5bb
Arqam Usman Ali <aa358@njit.edu>
2021-12-14 21:22:18 -0500

    Update multiply_test.py

--- tests/multiply_test.py
+++ tests/multiply_test.py
@@ -10,10 +10,3 @@ def test_multiply():
     nums = (3.0, 2.0)
     multiply = Multiplication(nums)
     assert multiply.get_result() == 6.0
-
-
-def test_multiply_incorrect():
-    """testing 3 multiplied by 2"""
-    nums = (3.0, 3.0)
-    multiply = Multiplication(nums)
-    assert multiply.get_result() != 6.0
