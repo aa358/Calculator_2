7fdc1c698b92c9e82363daded6e8e75b3612e29c
Arqam Usman Ali <aa358@njit.edu>
2021-12-14 21:40:20 -0500

    Update index_controller.py

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,8 +1,13 @@
-from app.controllers.controller import ControllerBase
+"""R"""
 from flask import render_template
+from app.controllers.controller import ControllerBase
 
 
 class IndexController(ControllerBase):
+    """R"""
+
     @staticmethod
     def get():
+        """R"""
         return render_template('index.html')
+
