ab1b650edd63c3a20417ce730b485ba02dc7c1dc
Arqam Usman Ali <aa358@njit.edu>
2021-12-14 21:39:54 -0500

    Update controller.py

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,4 +1,7 @@
-class ControllerBase:
+"""Docstring"""
+class ControllerBase: # pylint: disable=too-few-public-methods
+    """R"""
     @staticmethod
     def base():
+        """Docstring"""
         return True
